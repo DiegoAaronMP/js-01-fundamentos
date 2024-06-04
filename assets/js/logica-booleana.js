64afff7438fdac5856410ee3014076f0aef8b14c
DiegoAaronMP <daaron.macias@gmail.com>
2024-06-04 17:13:57 -0600

    Pro tip: Asignaciones con operadores

--- assets/js/logica-booleana.js
+++ assets/js/logica-booleana.js
@@ -32,3 +32,16 @@ console.log(false || false); // false
 console.log(regresaTrue() || regresaFalse()); // Regresa true true
 
 
+console.warn('Asignaciones');
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = true && 'Hola mundo'; // Hola mundo
+const a2 = 'Hola' && 'Mundo' && soyFalso && true; // falso
+const a3 = soyFalso || 'Ya no soy falso'; // 'Ya no soy falso'
+const a4 = soyFalso || soyUndefined || soyNull || 'Ya no soy falso de nuevo'; // Ya no soy falso de nuevo
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'Ya no soy falso de nuevo'; // true
+
+console.log({a1, a2, a3, a4}); 
