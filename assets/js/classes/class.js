856694811be22ed3315de6d5f97ad0b80ccb755c
DiegoAaronMP <daaron.macias@gmail.com>
2024-06-21 12:37:03 -0600

    Sets y Gets

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -5,6 +5,7 @@ class Persona {
     nombre = '';
     codigo = '';
     frase  = '';
+    comida = '';
 
     constructor(nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase'){
         this.nombre = nombre;
@@ -12,6 +13,15 @@ class Persona {
         this.frase  = frase;
     }
 
+    // Setters y Getters
+    // ! El set No puede tener el mismo nombre que la propiedad
+    set setComidaFavorita(comida) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita() {
+        return `La comida favorita de ${this.nombre} es ${this.comida}`;
+    }
     // Métodos
     quienSoy() {
         console.log(`Soy ${nombre} y mi identidad es ${this.codigo}`);
@@ -29,3 +39,5 @@ const ironman = new Persona('Tony Stark', 'Iron Man', 'Soy Iron Man');
 spiderman.quienSoy();
 ironman.miFrase();
 
+spiderman.setComidaFavorita = 'El pie de cereza de la tía May';
+console.log(spiderman.getComidaFavorita);
